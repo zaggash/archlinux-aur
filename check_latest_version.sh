e44cc9a6870766f2d661ae8be6dd9e693651fad7
Alexandre P <1724149+zaggash@users.noreply.github.com>
2020-06-11 01:51:30 +0200

    Rework build logic to add submodules

--- check_latest_version.sh
+++ check_latest_version.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
-set -e
+set -xe
 
-ENV_CI=$1
+PKG_DIR=$1
+[[ ! -d "$PKG_DIR" ]] && echo "$PKG_DIR is not a valid package folder." && exit 1
 
-source "$ENV_CI"
+pkg_name=$(basename "$PKG_DIR")
+
+if [[ -f "$PKG_DIR/.git" ]]
+then
+  platform="submodule"
+else
+  platform="$(sed -n 's/^url="\(.*\)"/\1/p' "$PKG_DIR/PKGBUILD" | cut -d'/' -f 3)"
+fi
 
 case "$platform" in
-  github)
+  submodule)
+    current_version=$(git -C "$PKG_DIR" log -1 --pretty=format:%H)
+    git -C "$PKG_DIR" pull -q
+    latest_version=$(git -C "$PKG_DIR" log -1 --pretty=format:%H)
+    ;;
+  github.com)
+    repo=$(sed -n 's/^url="\(.*\)"/\1/p' "$PKG_DIR/PKGBUILD" | cut -d'/' -f 4-5)
+    current_version=$(sed -n 's/^pkgver=\(.*\)/\1/p' "$PKG_DIR/PKGBUILD")
     latest_version=$(curl -skL \
-      "https://api.github.com/repos/$project/releases/latest" |\
+      "https://api.github.com/repos/$repo/releases/latest" |\
       jq -r '.tag_name' |\
       sed 's#[^0-9\.]*##g'
     )
+    sed -i "s#^pkgver.*#pkgver=$latest_version#" "$PKG_DIR/PKGBUILD"
     ;;
   *)
     echo "$platform is unknown !"
@@ -19,5 +35,17 @@ case "$platform" in
     ;;
 esac
 
-echo "$latest_version"
+if [[ "$latest_version" != "$current_version" ]]
+then
+  echo "New version found !"
+  touch commit_msg
+  touch new_versions_tag
+  echo "$pkg_name" >> new_versions_tag
+  git add "$PKG_DIR"
+  echo " * Bump $pkg_name version from $current_version to $latest_version" >> commit_msg
+else
+  echo "No update."
+fi
+
+
 exit 0
