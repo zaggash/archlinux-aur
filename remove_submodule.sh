3c6e4aa45d941ad8c651a5bad9bd8dff3ee1900d
Alexandre P <1724149+zaggash@users.noreply.github.com>
2020-06-23 23:23:09 +0200

    add remove_submodule.sh helper script [skip ci]

---
+++ remove_submodule.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+## execute the script again the good repo
+grep -sq archlinux-aur .git/config
+if [[ $? -ne 0 ]]
+  then
+    echo "Not in archlinux-aur Repo !"
+    exit 10
+fi
+
+## check number of arguments
+[[ $# -ne 1 ]] && echo "One submodule is needed as argument !" && exit 10
+
+## set argument as named variable
+MODULE=$1
+
+## check if argument is a submodule
+grep -qs "$MODULE.git" .gitmodules
+[[ $? -ne 0 ]] && echo "Submodule is not found !" && exit 30
+
+## run the script
+echo -e "\n * Updating the local repo"
+git pull
+git submodule update --init --recursive
+echo -e "\n * Removing the submodule $MODULE"
+git submodule deinit "$MODULE"
+git rm "$MODULE"
+echo -e "\n * add commit message"
+git commit -m "remove submodule $MODULE [skip ci]"
+echo -e "\n * removing local reference of the submodule"
+rm -rf ".git/modules/$MODULE"
+echo -e "\n ----------"
+echo "** $MODULE removed, if everything is fine, please proceed to the 'git push' **"
+echo " ----------"
